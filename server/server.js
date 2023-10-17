5fe717998523cbf619c8b9748a7afcbe1f4eb7ce
Bemon <tristan2107b@gmail.com>
2023-10-18 01:17:01 +1100

    heroku fix

--- server/server.js
+++ server/server.js
@@ -3,24 +3,20 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-
-// Connect to the database
 const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Handle database connection errors
 db.on('error', (error) => {
   console.error('Error connecting to the database:', error);
 });
 
 async function startServer() {
-  // Create new Apollo server and pass in schema data
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => authMiddleware({ req }),
+    context: authMiddleware,
     formatError: (err) => {
       console.error(err);
       return err;
@@ -28,27 +24,21 @@ async function startServer() {
   });
 
   try {
-    // Start the Apollo server
     await server.start();
-
-    // Integrate Apollo server with the Express application
     server.applyMiddleware({ app });
 
-    // Middleware for parsing body and JSON
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
-    // Serve up static assets if in production
+    // Serve up static assets
     if (process.env.NODE_ENV === 'production') {
-      app.use(express.static(path.join(__dirname, '../client/build')));
+      app.use(express.static(path.join(__dirname, '../client/dist')));
     }
 
-    // If no API routes are hit, send the React app
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+      res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
     });
 
-    // Once the database is open, start the server
     db.once('open', () => {
       app.listen(PORT, () => {
         console.log(`🌍 Now listening on localhost:${PORT}`);
@@ -61,5 +51,4 @@ async function startServer() {
   }
 }
 
-// Execute the server start function
 startServer();
